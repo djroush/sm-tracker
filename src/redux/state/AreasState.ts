d2822d5a35631eb35989103fc19a6dc006ac1184
Doug Roush <djroush@gmail.com>
2023-01-26 21:21:21 -0700

    Updated Area Names

--- src/redux/state/AreasState.ts
+++ src/redux/state/AreasState.ts
@@ -23,15 +23,15 @@ export type AreaState = {
 //TODO add max items here 
 export const defaultAreasState: AreaState[] = [
     { id: 0, value: 'UNKNOWN', bgColor: '#000000' , itemIds: [], maxItems: 0},
-    { id: 1, value: 'CRATERIA', bgColor: '#1C29C3', itemIds: [], maxItems: 17},
-    { id: 2, value: 'GRN BRIN', bgColor: '#008800', itemIds: [], maxItems: 16},
-    { id: 3, value: 'RED BRIN', bgColor: '#B71A12', itemIds: [], maxItems: 5 }, 
-    { id: 4, value: 'WEST MAR', bgColor: '#50C1A5', itemIds: [], maxItems: 7 },
+    { id: 1, value: 'CRATER', bgColor: '#1C29C3', itemIds: [], maxItems: 17},
+    { id: 2, value: 'GR BRIN', bgColor: '#008800', itemIds: [], maxItems: 16},
+    { id: 3, value: 'RED BRI', bgColor: '#B71A12', itemIds: [], maxItems: 5 },
+    { id: 4, value: 'W MARID', bgColor: '#50C1A5', itemIds: [], maxItems: 7 },
     { id: 5, value: 'UP NORF', bgColor: '#E49B15', itemIds: [], maxItems: 15 },
-    { id: 6, value: 'CROC', bgColor: '#F0280F', itemIds: [], maxItems: 5 }, 
+    { id: 6, value: 'CROC', bgColor: '#F0280F', itemIds: [], maxItems: 5 },
     { id: 7, value: 'KRAID', bgColor: '#73C403', itemIds: [], maxItems: 3, bossId:0},
-    { id: 8, value: 'SHIP', bgColor: '#D5C907', itemIds: [], maxItems: 11, bossId:0},
-    { id: 9, value: 'EAST MAR', bgColor: '#B076FF', itemIds: [], maxItems: 11, bossId:0},
+    { id: 8, value: 'W SHIP', bgColor: '#D5C907', itemIds: [], maxItems: 11, bossId:0},
+    { id: 9, value: 'E MARID', bgColor: '#B076FF', itemIds: [], maxItems: 11, bossId:0},
     { id: 10, value: 'LO NORF', bgColor: '#A4682D', itemIds: [], maxItems: 10, bossId:0},
     { id: 11, value: 'TOURIAN', bgColor: '#818181',  itemIds: [], maxItems: 0}
 ]
